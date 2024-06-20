d611813830d4bf5e6bcf559cc423ebd6d1e87a49
natemccolm <natemccolm@gmail.com>
2024-06-20 11:31:29 -0600

    color_changes

---
+++ week02/js_practice.js
@@ -0,0 +1,19 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+
+for (let i=0; i<30; i++) {
+    if (studentReport[i]<limit){
+        console.log(studentReport[i])
+
+
+    }
+}
+
+let i=0;
+while (i<studentReport.length){
+    if (studentReport[i]<limit)
+        console.log(studentReport[i])
+}
+
