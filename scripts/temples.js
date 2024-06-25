73aadbe48cb5e2c0fc1a6629bde41d12b15678f6
natemccolm <natemccolm@gmail.com>
2024-06-24 18:14:23 -0600

    hamburger fix

--- scripts/temples.js
+++ scripts/temples.js
@@ -12,3 +12,12 @@ const todaysdate=new Intl.DateTimeFormat(
 
 currentyear.innerHTML='&copy; ' + year.getFullYear() + ' Nate McColm,IDAHO'
 lastmodified.innerHTML=todaysdate
+
+
+const mainnav=document.querySelector('.nav')
+const hambutton=document.querySelector('#menu')
+
+hambutton.addEventListener('click', () =>{
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
