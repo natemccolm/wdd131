5f71eb8e3e2ce5251202b6f56316d0955452811b
natemccolm <natemccolm@gmail.com>
2024-07-14 17:52:35 -0600

    jsupdates

--- scripts/form.js
+++ scripts/form.js
@@ -13,35 +13,44 @@ const todaysdate=new Intl.DateTimeFormat(
 currentyear.innerHTML='&copy; ' + year.getFullYear() + ' Nate McColm,IDAHO'
 lastmodified.innerHTML=todaysdate
 
-const pullList=document.querySelector('product')
+const pullList=document.querySelector('#productID')
 
-pullList.innerHTML=products
+
 
 
 const products = [
 	{
-	  id: fc-1888,
+	  id: 'fc-1888',
 	  name: "flux capacitor",
 	  averagerating: 4.5
 	},
 	{
-	  id: fc-2050,
+	  id: 'fc-2050',
 	  name: "power laces",
 	  averagerating: 4.7
 	},
 	{
-	  id: fs-1987,
+	  id: 'fs-1987',
 	  name: "time circuits",
 	  averagerating: 3.5
 	},
 	{
-	  id: ac-2000,
+	  id: 'ac-2000',
 	  name: "low voltage reactor",
 	  averagerating: 3.9
 	},
 	{
-	  id: jj-1969,
+	  id: 'jj-1969',
 	  name: "warp equalizer",
 	  averagerating: 5.0
 	}
   ];
+
+  for (element in products) {
+	var opt=document.createElement('option')
+	opt.value=products[element].id;
+	opt.innerHTML=products[element].name;
+	pullList.appendChild(opt)
+  }
+
+  
